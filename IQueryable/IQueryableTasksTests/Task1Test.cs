e1f0a4747e5566222571368d9cb2a3f92e28f378
VladrSp <Vladrsp@gmail.com>
2017-12-14 12:53:40 +0000

    remove comments

--- IQueryable/IQueryableTasksTests/Task1Test.cs
+++ IQueryable/IQueryableTasksTests/Task1Test.cs
@@ -8,57 +8,6 @@ using System.Collections.ObjectModel;
 
 namespace IQueryableTasksTests
 {
-    //public class Parameters : ExpressionVisitor
-    //{
-    //    Dictionary<string, object> parameters;
-    //    public Parameters(Dictionary<string,object> parameters) : base()
-    //    {
-    //        this.parameters = parameters;
-    //    }
-    //    protected override Expression VisitBinary(BinaryExpression node)
-    //    {
-    //        if (node.NodeType == ExpressionType.Subtract)
-    //        {
-    //            ParameterExpression param = null;
-    //            ConstantExpression constant = null;
-    //            if (node.Left.NodeType == ExpressionType.Parameter)
-    //            {
-    //                param = (ParameterExpression)node.Left;
-    //            }
-    //            else if (node.Left.NodeType == ExpressionType.Constant)
-    //            {
-    //                constant = (ConstantExpression)node.Left;
-    //            }
-
-    //            if (node.Right.NodeType == ExpressionType.Parameter)
-    //            {
-    //                param = (ParameterExpression)node.Right;
-    //            }
-    //            else if (node.Right.NodeType == ExpressionType.Constant)
-    //            {
-    //                constant = (ConstantExpression)node.Right;
-    //            }
-
-    //            if (param != null && constant != null && constant.Type == typeof(int) && parameters.ContainsKey(param.Name))
-    //            {
-    //                var exp3 = Expression.Lambda(
-    //                    Expression.New(
-    //                        obj.GetType().GetConstructors()[0],
-    //                        Expression.Constant("Alex"), Expression.Constant(17)
-    //                        ));
-                
-    //            }
-    //            return base.VisitBinary(node);
-    //        }
-    //    }
-
-    //    private string VisitBinary(BinaryExpression binary, string @operator, LambdaExpression expression)
-    //    {
-    //        return $"{@operator}({this.VisitNode(binary.Left, expression)}, {this.VisitNode(binary.Right, expression)})";            
-    //    }        
-    //}
-
-     
     [TestClass]
     public class Task1Test
     {
