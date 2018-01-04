f69fbab81ec6b4142b61f7a67e746fda352112f5
VladrSp <Vladrsp@gmail.com>
2018-01-04 10:03:18 +0000

    unification Visitors

---
+++ IQueryable/IQueryableTasks/Task1/ParameterTransformVisitor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IQueryableTasks.Task1
+{
+    public class ParameterTransformVisitor : ExpressionVisitor
+    {
+        private Dictionary<string, string> parameters;
+        public ParameterTransformVisitor(Dictionary<string, string> parameters)
+        {
+            this.parameters = parameters ?? new Dictionary<string, string>();
+
+        }
+        public ParameterTransformVisitor():base()
+        {
+            this.parameters = new Dictionary<string, string>();
+        }
+        
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.NodeType == ExpressionType.Subtract || node.NodeType == ExpressionType.Add)
+            {
+                ParameterExpression param = null;
+                ConstantExpression constant = null;
+                if (node.Left.NodeType == ExpressionType.Parameter)
+                {
+                    param = ReplaceParam((ParameterExpression)node.Left);
+
+                }
+                else if (node.Left.NodeType == ExpressionType.Constant)
+                {
+                    constant = (ConstantExpression)node.Left;
+                }
+
+                if (node.Right.NodeType == ExpressionType.Parameter)
+                {
+                    param = ReplaceParam((ParameterExpression)node.Right);
+
+                }
+                else if (node.Right.NodeType == ExpressionType.Constant)
+                {
+                    constant = (ConstantExpression)node.Right;
+                }
+
+                if (node.NodeType == ExpressionType.Subtract && param != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1)
+                {
+                    return Expression.Decrement(param);
+                }
+                else if (node.NodeType == ExpressionType.Add && param != null && constant != null && constant.Type == typeof(int) && (int)constant.Value == 1)
+                {
+                    return Expression.Increment(param);
+                }
+
+            }
+            return base.VisitBinary(node);
+        }
+
+        private ParameterExpression ReplaceParam(ParameterExpression param)
+        {
+            string value = null;
+            if (parameters.Any() && parameters.TryGetValue(param.Name, out value))
+            {
+                return Expression.Parameter(param.Type, value);
+            }
+            else
+            {
+                return param;
+            }
+        }
+
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            ParameterExpression replacement = ReplaceParam(node);
+            return base.VisitParameter(replacement);
+        }
+
+    }
+}
--- IQueryable/IQueryableTasksTests/Task1Test.cs
+++ IQueryable/IQueryableTasksTests/Task1Test.cs
@@ -15,28 +15,34 @@ namespace IQueryableTasksTests
         public void TestIncrement()
         {
             Expression<Func<int, int>> source_exp = (a) => a + (a + 1) * (a + 5) * (a + 1);
-            var result = new AddToIncrementTransform().VisitAndConvert(source_exp,"");
+            var result = new ParameterTransformVisitor().VisitAndConvert(source_exp,"");
             var result_value = result.Compile().Invoke(2);
-            Assert.AreEqual(65, result_value);
-            
+            Assert.AreEqual(65, result_value);            
         }
         [TestMethod]
         public void TestDecrement()
         {
-            Expression<Func<int, int>> source_exp = (a) => a + (a - 1) * (a - 5) * (a - 1);
-            var result = new AddToDecrementTransform().VisitAndConvert(source_exp, "");
+            Expression<Func<int, int>> source_exp = (a) => (a + 1) * (a - 5) * (a - 1);
+            var result = new ParameterTransformVisitor().VisitAndConvert(source_exp, "");
             var result_value = result.Compile().Invoke(2);
-            Assert.AreEqual(-1, result_value);
+            Assert.AreEqual(-9, result_value);
         }
         
         [TestMethod]
         public void TestParameters()
         {
-            Expression<Func<int,int,int>> source_exp = (a,b) => a + (a - 1) * (b - 5) * (a - 1);
-            var param = new Dictionary<string, string> { {"a","c" }, { "b", "d" } };
-            var result = new ReplaceParametersTypeVisitor().VisitReplaceParameters(source_exp,param);
+            Expression<Func<int,int,int>> source_exp = (a,b) => a + (a - 1) * (b - 5) * (a + 1);
+            var param = new Dictionary<string, string> { {"a","c" } };
+            var result = new ParameterTransformVisitor(param).VisitAndConvert(source_exp,"");
             var result_text = result.ToString();
-            Assert.AreEqual("(c, d) => (c + (((c - 1) * (d - 5)) * (c - 1)))", result_text);
+            Assert.AreEqual("(c, b) => (c + ((Decrement(c) * (b - 5)) * Increment(c)))", result_text);
+
+            var arg = Expression.Parameter(typeof(int), "c");
+            var exp = Expression.Add(arg, Expression.Constant(7));
+            Func<int, int> result_comp = Expression.Lambda<Func<int, int>>(exp, arg).Compile();
+
+            var result_value = result_comp.Invoke(3);
+            Assert.AreEqual(10, result_value);
         }
     }
     
